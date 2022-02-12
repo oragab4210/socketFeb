bfb8ea4f1ad52a27c14ec929add5efe8ff29e42c
Omar Ragab <oragab1518@gmail.com>
2022-02-12 14:04:23 -0700

    test2

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,10 @@ function App() {
   // socketRef.current = socketIOClient(SOCKET_SERVER_URL);
 
   useEffect(() => {
-    socketRef.current = socketIOClient(SOCKET_SERVER_URL);
+    socketRef.current = socketIOClient(SOCKET_SERVER_URL, {
+      transports: ["websocket"],
+      rejectUnauthorized: false,
+    });
     console.log("working", socketRef.current);
     // socketRef.current = socketIOClient(SOCKET_SERVER_URL);
     socketRef.current.emit("login", { userId: 5 });
